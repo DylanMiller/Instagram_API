5cedd742d7c28171b82624c9c8de259dbe1ec97f
dylanmiller <dmiller3199@gmail.com>
2015-05-06 08:37:01 -0700

    milestarn 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	session_start();
+
+	define('client_id', '477393f935964621b84185fb1c272017');
+	define('client_Secret', 'b3b9843feb9d412aab05bc61367737ef');
+	define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
+	define('ImageDirectory', 'pictographs/');
+
+?>
+
+
+
+<!---CLIENT INFO
+CLIENT ID 477393f935964621b84185fb1c272017
+CLIENT SECRET b3b9843feb9d412aab05bc61367737ef
+WEBSITE URL http://localhost:8888/appacademyapi/index.php
+REDIRECT URI http://localhost:8888/appacademyapi/index.php
