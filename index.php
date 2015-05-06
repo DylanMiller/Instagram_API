05c7eef213cb1e8ff236277b3418923ee65a492d
dylanmiller <dmiller3199@gmail.com>
2015-05-06 09:15:16 -0700

    milestone 3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	ini_set('default_socket_timeout', 300);
 	session_start();
 
-	define('client_id', '477393f935964621b84185fb1c272017');
+	define('client_ID', '477393f935964621b84185fb1c272017');
 	define('client_Secret', 'b3b9843feb9d412aab05bc61367737ef');
 	define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
 	define('ImageDirectory', 'pictographs/');
@@ -11,9 +11,18 @@
 ?>
 
 
-
-<!---CLIENT INFO
-CLIENT ID 477393f935964621b84185fb1c272017
-CLIENT SECRET b3b9843feb9d412aab05bc61367737ef
-WEBSITE URL http://localhost:8888/appacademyapi/index.php
-REDIRECT URI http://localhost:8888/appacademyapi/index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="">
+	<link rel="author" href="humans.txt">
+</head>
+<body>
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
+	<script type="js/main.js"></script>
+</body>
+</html>
